9cc6f15ea1c7b2b66971d8850af95340a39acd5e
Frank Alayza <stephanoherrera4444@gmail.com>
2023-04-27 18:15:55 -0500

    Ending the Products Section

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+function scrolll() {
+    var left = document.querySelector(".products-bot");
+    left.scrollBy(-250, 0)
+}
+function scrollr() {
+    var right = document.querySelector(".products-bot");
+    right.scrollBy(250, 0)
+}
